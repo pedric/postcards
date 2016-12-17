796983978b811dd24aee78159ed9780187766b3d
Fredrik Larsson <epost.larsson@gmail.com>
2016-12-17 18:10:04 +0100

    Test to fix public version crash

--- server.js
+++ server.js
@@ -65,7 +65,4 @@ app.put('/postcards/:id', function(req, res) {
 
 var port = Number(process.env.port || 3000);
 app.listen(port);
-console.log("Server listens on port " + port);
-
-module.exports = app;
 
